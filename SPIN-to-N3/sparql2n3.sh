7d9418d022deb4d5ca0fe9f6c02da70c367cf5a2
William Van Woensel <wvw@Williams-MBP.lan>
2023-03-02 12:16:48 -0500

    only print selected group-vars | shell script for easier testing

---
+++ SPIN-to-N3/sparql2n3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+query=$1
+data=$2
+
+spin_file="out/tmp.spin"
+echo -e ">> getting spin <<"
+$(java -jar sparql2spin.jar -sparql $query > $spin_file)
+echo -e "(stored at $spin_file)"
+
+n3_file="out/tmp.n3"
+echo -e "\n\n>> getting n3 <<"
+eye out/tmp.spin auxiliary-files/aux.n3 \
+    --query queries/query_general.n3 --nope --quantify http://eyereasoner.github.io/.well-known/genid/ \
+    > $n3_file
+echo -e "(stored at $n3_file)"
+
+echo -e "\n\n>> executing n3 <<"
+eye auxiliary-files/aux-aggr.n3 \
+    $data out/tmp.n3 --nope --pass-only-new
