388f0293d86ad4157fba53382fc2fbf035b009ed
william-vw <wvw@Williams-MacBook-Pro.local>
2023-08-15 13:25:32 -0400

    fix example

--- SPIN-to-N3/test/canonical/canonical_test.py
+++ SPIN-to-N3/test/canonical/canonical_test.py
@@ -1,18 +1,16 @@
 from rdflib import Graph, compare
 import time
 
-path = "SPARQL-to-N3/SPIN-to-N3/test/canonical/"
 for file in [ "test1.ttl", "test2.ttl", "test3.ttl" ]:
     print("file:", file)
 
     graph1 = Graph()
-    graph1.parse(f"{path}{file}", format='ttl')
+    graph1.parse(file, format='ttl')
 
     start = time.time()
 
     iso1 = compare.to_isomorphic(graph1)
-    iso2 = compare.to_isomorphic(graph1)
-    equal = (iso1 == iso2)
+    equal = (iso1 == iso1)
 
     end = time.time()
     print("isomorphic:", (end-start))
