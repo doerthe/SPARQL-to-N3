b7783d781fc88ad3af155041a1f9c068d726c6f5
william-vw <wvw@Williams-MBP.lan>
2023-08-20 13:09:04 -0400

    initial commit

--- SPIN-to-N3/test/run/run_spin3.sh
+++ SPIN-to-N3/test/run/run_spin3.sh
@@ -1,29 +1,59 @@
 #!/bin/bash
 
-query=$1
-data=$2
-verbose=$3
-result_file=$4
+sparql=$1
+query=$2
+data=$3
+verbose=$4
+result_file=$5
+
+gen_spin () {
+    local time=$( TIMEFORMAT="%R"; { time { java -jar sparql2spin.jar -sparql $1 -multi > $2; } } 2>&1 )
+    # echo "java -jar sparql2`spin.jar -sparql $1 -multi"
+    if [[ $verbose == "true" ]]; then
+        echo -e "(stored at $2)"
+    fi
+    local __resultVar=$3
+    eval $__resultVar="$time"
+}
+
+gen_n3() {
+    local time=$( TIMEFORMAT="%R"; { time { eye $1 ../../auxiliary-files/aux.n3 --query ../../queries/query_general.n3 --nope --quantify http://eyereasoner.github.io/.well-known/genid/ > $2 2>/dev/null; } } 2>&1 )
+    # echo "eye $1 ../../auxiliary-files/aux.n3 --query ../../queries/query_general.n3 --nope --quantify http://eyereasoner.github.io/.well-known/genid/"
+    if [[ $verbose == "true" ]]; then
+        echo -e "(stored at $2)"
+    fi
+    local __resultVar=$3
+    eval $__resultVar="$time"
+}
 
 mkdir -p results
 mkdir -p tmp
 
-spin_file="tmp/query.spin"
 if [[ $verbose == "true" ]]; then
     echo -e "> getting spin <"
 fi
-time_gen_spin=$( TIMEFORMAT="%R"; { time { java -jar sparql2spin.jar -sparql $query -multi > $spin_file; } } 2>&1 )
-if [[ $verbose == "true" ]]; then
-    echo -e "(stored at $spin_file)"
+
+sparql_spin_file="tmp/sparql.spin"
+gen_spin $sparql $sparql_spin_file time_sparql_spin
+
+if [[ "$sparql" != "$query" ]]; then
+    query_spin_file="tmp/query.spin"
+    gen_spin $query $query_spin_file time_query_spin
+else
+    query_spin_file=$sparql_spin_file
 fi
 
-n3_file="tmp/n3query.n3"
 if [[ $verbose == "true" ]]; then
     echo -e "\n> getting n3 <"
 fi
-time_gen_n3=$( TIMEFORMAT="%R"; { time { eye $spin_file ../../auxiliary-files/aux.n3 --query ../../queries/query_general.n3 --nope --quantify http://eyereasoner.github.io/.well-known/genid/ > $n3_file 2>/dev/null; } } 2>&1 )
-if [[ $verbose == "true" ]]; then
-    echo -e "(stored at $n3_file)"
+sparql_n3_file="tmp/sparql.n3"
+gen_n3 $sparql_spin_file $sparql_n3_file time_sparql_n3
+
+if [[ "$sparql" != "$query" ]]; then
+    query_n3_file="tmp/query.n3"
+    gen_n3 $query_spin_file $query_n3_file time_query_n3
+else
+    query_n3_file=$sparql_n3_file
 fi
 
 if [[ -z $result_file ]]; then
@@ -32,11 +62,11 @@ fi
 if [[ $verbose == "true" ]]; then
     echo -e "\n> executing n3 <"
 fi
-# including query as data ( new ):
-time_exec_n3=$( TIMEFORMAT="%R"; { time { eye ../../auxiliary-files/runtime.n3 $data $n3_file --query $n3_file --nope > $result_file 2>/dev/null; } } 2>&1 )
-# not including query as data (before):
-#time_exec_n3=$( TIMEFORMAT="%R"; { time { eye ../../auxiliary-files/runtime.n3 $data --query $n3_file --nope > $result_file 2>/dev/null; } } 2>&1 )
+time_exec_n3=$( TIMEFORMAT="%R"; { time { eye ../../auxiliary-files/runtime.n3 $data $sparql_n3_file --query $query_n3_file --nope > $result_file 2>/dev/null; } } 2>&1 )
+echo "eye ../../auxiliary-files/runtime.n3 $data $sparql_n3_file --query $query_n3_file --nope"
+# not including query as data:
+#time_exec_n3=$( TIMEFORMAT="%R"; { time { eye ../../auxiliary-files/runtime.n3 $data --query $sparql_n3_file --nope > $result_file 2>/dev/null; } } 2>&1 )
 if [[ $verbose == "true" ]]; then
     echo -e "(stored results at $result_file)"
 fi
-echo -e "\ngenerate spin: $time_gen_spin\ngenerate n3: $time_gen_n3\nexec n3: $time_exec_n3"
+echo -e "\ngenerate spin: $time_sparql_spin, $time_query_spin\ngenerate n3: $time_sparql_n3, $time_query_n3\nexec n3: $time_exec_n3"
