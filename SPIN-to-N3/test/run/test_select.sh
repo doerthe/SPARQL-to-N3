09e4e0751771b2bbd901de03711e1fbafa744b78
Dörthe Arndt <dorthearndt@Dorthes-MacBook-Pro.local>
2024-10-09 15:07:00 +0200

    extra argument for run_sin3

--- SPIN-to-N3/test/run/test_select.sh
+++ SPIN-to-N3/test/run/test_select.sh
@@ -21,7 +21,7 @@ echo -e "(executing N3 queries)"
 n3_res_nt0="results/spin3_results_init.nt"
 n3_res_csv="results/spin3_results.csv"
 # (get rdf output)
-./run_spin3.sh $query $data false $n3_res_nt0 false
+./run_spin3.sh $query $data false false  false $n3_res_nt0
 # (convert rdf output to csv)
 eye $n3_res_nt0 --nope ../../auxiliary-files/csv-convert.n3 --strings > $n3_res_csv 2>/dev/null
 
