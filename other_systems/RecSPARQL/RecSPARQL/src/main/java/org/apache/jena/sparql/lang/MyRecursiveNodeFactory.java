988980e17d27dad627bfa1b2202edaffebfe2fb3
william-vw <wvw@Williams-MBP.lan>
2024-12-10 07:44:10 -0500

    adding all nmo queries

--- other_systems/RecSPARQL/RecSPARQL/src/main/java/org/apache/jena/sparql/lang/MyRecursiveNodeFactory.java
+++
@@ -1,9 +0,0 @@
-package org.apache.jena.sparql.lang;
-
-public class MyRecursiveNodeFactory extends RecursiveNodeFactory {
-
-	@Override
-	public IRecursiveNode createRecursiveNode() {
-		return new MyRecursiveNode();
-	}
-}
--- other_systems/SparqLog/SparqLog3/src/main/java/Translation/SkolemGenerator.java
+++
@@ -1,7 +0,0 @@
-package Translation;
-
-import java.util.List;
-
-public interface SkolemGenerator {
-    public String generateSkolemString(List<MyNode> varNames, List<Integer> ids, String rule_id, String addedString);
-}
