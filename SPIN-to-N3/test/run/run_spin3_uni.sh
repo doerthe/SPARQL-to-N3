abc2e68aaf718ca000ecaf8ffbe91313dddb3252
doerthe <mailandoerthe@gmail.com>
2024-12-10 18:46:04 +0100

    support for one-to-one translation between SPARQL and N3 (run_spin3_uni.sh)

---
+++ SPIN-to-N3/test/run/run_spin3_uni.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+query=$1
+data=$2
+verbose=$3
+recursion=$4
+preproc=$5
+result_file=$6
+
+mkdir -p results
+mkdir -p tmp
+
+nt_file=$data
+if [[ $preproc == "true" ]]; then
+    
+    if [[ ! "${data}" =~ \.nt$ ]]; then
+        nt_file="tmp/ntriples.nt"
+        if [[ $verbose == "true" ]]; then
+            echo -e "> getting n-triples data <"
+        fi
+        time_gen_nt=$( TIMEFORMAT="%R"; { time { java -jar turtle2nt.jar -turtle $data  > $nt_file; } } 2>&1 )
+        if [[ $verbose == "true" ]]; then
+            echo -e "(stored at $nt_file)"
+        fi
+    fi
+    if [[ $verbose == "true" ]]; then
+        echo -e "> preprocessing n-triples <"
+    fi
+    start=$(gdate +%s%N)
+    sed -i'' -e 's|<http://www.w3.org/1999/02/22-rdf-syntax-ns#first>|<http://www.w3.org/1999/02/22-rdf-syntax-ns#f1rst>|g' $nt_file
+    sed -i'' -e 's|<http://www.w3.org/1999/02/22-rdf-syntax-ns#rest>|<http://www.w3.org/1999/02/22-rdf-syntax-ns#r3st>|g' $nt_file
+    end=$(gdate +%s%N)
+    time_preproc=$(bc -l <<< "scale = 2; ($end-$start)/1000000000")
+fi
+
+spin_file="tmp/query.spin"
+if [[ $verbose == "true" ]]; then
+    echo -e "\n> getting spin <"
+fi
+time_gen_spin=$( TIMEFORMAT="%R"; { time { java -jar sparql2spin.jar -sparql $query  > $spin_file; } } 2>&1 )
+if [[ $verbose == "true" ]]; then
+    echo -e "(stored at $spin_file)"
+fi
+
+n3_file="tmp/n3query.n3"
+n3_stderr="tmp/n3query-output.txt"
+if [[ $verbose == "true" ]]; then
+    echo -e "\n> getting n3 rules <"
+fi
+time_gen_n3=$( TIMEFORMAT="%R"; { time { eye $spin_file ../../auxiliary-files/aux_iso.n3 --query ../../queries/query_general.n3 --nope --quantify https://eyereasoner.github.io/.well-known/genid/ > $n3_file 2>$n3_stderr; } } 2>&1 )
+if [[ $verbose == "true" ]]; then
+    echo -e "(stored at $n3_file)"
+fi
+
+sed -i'' -e 's|rdf:first|rdf:f1rst|g' $n3_file
+sed -i'' -e 's|rdf:rest|rdf:r3st|g' $n3_file
+
+if [[ -z $result_file ]]; then
+    result_file="tmp/results.n3"
+fi
+if [[ $verbose == "true" ]]; then
+    echo -e "\n> executing n3 <"
+fi
+
+if [[ $recursion == "true" ]]; then
+    # including query as data (new):
+    time_exec_n3=$( TIMEFORMAT="%R"; { time { eye ../../auxiliary-files/runtime_iso.n3 $n3_file --turtle $nt_file --pass-only-new --nope > $result_file 2>/dev/null; } } 2>&1 )
+else
+    # not including query as data (before):
+    time_exec_n3=$( TIMEFORMAT="%R"; { time { eye ../../auxiliary-files/runtime_iso.n3 --turtle $nt_file --query $n3_file --nope > $result_file 2>/dev/null; } } 2>&1 )
+fi
+
+if [[ $verbose == "true" ]]; then
+    echo -e "(stored results at $result_file)"
+fi
+
+if [[ $verbose == "true" ]]; then
+    if [[ $preproc == "true" ]]; then
+        echo -e "\ngenerate nt: $time_gen_nt s\npreproc: $time_preproc s"
+    fi
+    echo -e "\ngenerate spin: $time_gen_spin s\ngenerate n3: $time_gen_n3 s\nexec n3: $time_exec_n3 s"
+fi
