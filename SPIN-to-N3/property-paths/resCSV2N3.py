d0b9ef8baccb2b1c39217eebd4e646872044370b
william-vw <wvw@Williams-MBP.lan>
2025-05-21 10:23:42 -0400

    restructure notebook

--- SPIN-to-N3/property-paths/resCSV2N3.py
+++ SPIN-to-N3/property-paths/resCSV2N3.py
@@ -14,17 +14,23 @@ import rdflib
 def guess_term_type(string):
     if re.match(r'^[a-zA-Z]+://', string):
         return 'iri'
-
-    if string.startswith('_:'):
+    elif string.startswith('_:'):
         return 'bnode'
-
     return 'literal'
 
-def toProperty(label):
-    label = re.sub(r"[^\w]", " ", label)
-    label = re.sub("([a-z])([A-Z])", "\\1 \\2", label)
-    label = label.split(" ")
-    return "".join([label[0].lower()] + [x.capitalize() for x in label[1:]])
+def term(str):
+    str = str.strip()
+    term_type = guess_term_type(str)
+    match(term_type):
+        case 'bnode':
+            return rdflib.BNode(str[2:])
+        case 'iri':
+            return rdflib.URIRef(str)
+        case _:
+            return rdflib.Literal(str)
+        
+def list(elements):
+    return "(" + " ".join([ element.n3() if isinstance(element, rdflib.term.Node) else element for element in elements ]) + " )"
 
 def csv_reader(csv_data, dialect=csv.excel, **kwargs):
     csv_reader = csv.reader(csv_data, dialect=dialect, **kwargs)
@@ -33,21 +39,16 @@ def csv_reader(csv_data, dialect=csv.excel, **kwargs):
 
 class RESRDF:
     def __init__(self, out, ordered=False):
-        self.BASE = rdflib.Namespace("http://example.org/")
-        self.PROPBASE = rdflib.Namespace("http://example.org/")
+        self.BASE = rdflib.Namespace("http://example.org/gmark/")
+        self.RES_PROP = self.BASE['result']
         self.IDENT = "auto"
-        self.DEFAULT = None
-        self.COLUMNS = {}
         self.OUT = out
         self.ORDERED = ordered
         self.triples = 0
 
-    def triple(self, s, p, o):
-        if guess_term_type(o) == 'bnode':
-            o = rdflib.BNode(o[2:])
-        elif guess_term_type(o) == 'iri':
-            o = rdflib.URIRef(o)
-        self.OUT.write("%s %s %s .\n" % (s.n3(), p.n3(), o.n3()))
+    def result(self, subj, cells):
+        o = list([list(cell) for cell in cells])
+        self.OUT.write("%s %s %s .\n" % (subj.n3(), self.RES_PROP.n3(), o))
         self.triples += 1
 
     def convert(self, csvreader):
@@ -60,7 +61,6 @@ class RESRDF:
         except:
             return
     
-        headers = dict(enumerate([self.PROPBASE[toProperty(x)] for x in header_labels]))
         rows = 0
         for l_ in csvreader:
             try:
@@ -69,24 +69,13 @@ class RESRDF:
                         subj = self.BASE["%d" % rows]
                     else:
                         subj = rdflib.BNode()
-                for i, x in enumerate(l_):
-                    x = x.strip()
-                    if x != "":
-                        if self.COLUMNS.get(i, self.DEFAULT) == "ignore":
-                            continue
-                        try:
-                            o = self.COLUMNS.get(i, rdflib.Literal)(x)
-                            if isinstance(o, list):
-                                for _o in o:
-                                    self.triple(subj, headers[i], _o)
-                            else:
-                                self.triple(subj, headers[i], o)
-                        except Exception as e:
-                            warnings.warn(
-                                "Could not process value for column "
-                                + "%d:%s in row %d, ignoring: %s "
-                                % (i, headers[i], rows, e.message)
-                            )
+                else:
+                    sys.stderr.write("unknown IDENT:", self.IDENT)
+                    return
+                
+                cells = [(term(header_labels[i]), term(x)) for i, x in enumerate(l_)]
+                self.result(subj, cells)
+                
                 rows += 1
                 if rows % 100000 == 0:
                     sys.stderr.write(
