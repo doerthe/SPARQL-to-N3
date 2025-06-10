ff30edc30943744c456d81978a58490f3e120756
william-vw <wvw@Williams-MacBook-Pro.local>
2025-06-10 10:37:19 -0400

    new results

--- SPIN-to-N3/property-paths/resCSV2N3.py
+++ SPIN-to-N3/property-paths/resCSV2N3.py
@@ -11,12 +11,14 @@ import warnings
 import argparse
 import rdflib
 
+
 def n3_term_type(string):
     if re.match(r'^[a-zA-Z]+://', string):
         return 'iri'
     elif string.startswith('_:'):
         return 'bnode'
     return 'literal'
+
 
 def n3_term(string):
     string = string.strip()
@@ -29,14 +31,17 @@ def n3_term(string):
         case _:
             term = rdflib.Literal(string)
     return term
-        
+
+
 def n3_list(elements):
-    return "(" + " ".join([ element.n3() if isinstance(element, rdflib.term.Node) else element for element in elements ]) + " )"
+    return "(" + " ".join([element.n3() if isinstance(element, rdflib.term.Node) else element for element in elements]) + " )"
+
 
 def csv_reader(csv_data, dialect=csv.excel, **kwargs):
     csv_reader = csv.reader(csv_data, dialect=dialect, **kwargs)
     for row in csv_reader:
         yield row
+
 
 class RESRDF:
     def __init__(self, out, ordered=False):
@@ -61,7 +66,7 @@ class RESRDF:
             header_labels = list(next(csvreader))
         except:
             return
-    
+
         rows = 0
         for l_ in csvreader:
             try:
@@ -73,10 +78,11 @@ class RESRDF:
                 else:
                     sys.stderr.write("unknown IDENT:", self.IDENT)
                     return
-                
-                cells = [(n3_term(header_labels[i]), n3_term(x)) for i, x in enumerate(l_)]
+
+                cells = [(n3_term(header_labels[i]), n3_term(x))
+                         for i, x in enumerate(l_)]
                 self.result(subj, cells)
-                
+
                 rows += 1
                 if rows % 100000 == 0:
                     sys.stderr.write(
@@ -90,11 +96,22 @@ class RESRDF:
         # sys.stderr.write("Converted %d rows into %d triples.\n" % (rows, self.triples))
         # sys.stderr.write("Took %.2f seconds.\n" % (time.time() - start))
 
-def convert(file, ordered, out):
-    out = open(out, 'w')
-    if ordered:
-        csv2rdf = RESRDF(out=out, ordered=True)
-    else:
-        csv2rdf = RESRDF(out=out)
 
-    csv2rdf.convert(csv_reader(open(file, 'r')))
+def convert(file, ordered, out):
+    with open(out, 'w') as out, open(file, 'r') as input:
+        if ordered:
+            csv2rdf = RESRDF(out=out, ordered=True)
+        else:
+            csv2rdf = RESRDF(out=out)
+
+        # ASK
+        contents = input.read()
+        if match := re.search("result: (true|false)", contents):
+            out.write(f"PREFIX : <http://example.org/gmark/> \n\n :x :result {match.group(1)} .")
+        # SELECT 
+        else:
+            with open(file, 'r') as input:
+                csv2rdf.convert(csv_reader(input))
+
+convert("/Users/wvw/git/n3/sparql2n3/SPARQL-to-N3/gmark_50_new/mix/results/query-15.csv",
+        False, "/Users/wvw/git/n3/sparql2n3/SPARQL-to-N3/gmark_50_new/mix/results/query-15.n3")
